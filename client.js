d251735e5b1bdbe6d6e338bb462ebc13e0ee4e35
Sean Oyler <xrysen@gmail.com>
2020-11-04 20:07:35 +0000

    Started adding basics of movement

--- client.js
+++ client.js
@@ -3,23 +3,22 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
   // Recieve data from server
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
-});
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
 
-conn.on('connect', () => {
-  console.log("Connected");
-  conn.write("Name: SMO");
-});
-
+  conn.on('connect', () => {
+    console.log("Connected");
+    conn.write("Name: SMO");
+  });
   return conn;
 }
 
