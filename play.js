dad71c7693bb8e8b952da20551c45427170d45fb
Sean Oyler <xrysen@gmail.com>
2020-11-04 20:20:39 +0000

    Allow user to exit game with Ctrl+C

--- play.js
+++ play.js
@@ -1,4 +1,26 @@
 const connect = require("./client");
 
 console.log('Connecting ...');
+
+// Setup user interface Specifically so that stdin can handle user input
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  handleUserInput(stdin);
+  return stdin;
+};
+
+const handleUserInput = (stdin) => {
+  stdin.on('data', (key) => {
+    if(key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
+setupInput();
+
 connect();
